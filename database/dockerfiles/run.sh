9f79406dac615983791fc6da0a04bf021fa4387c
Stefan Walker <stefanwalker@Stefans-MacBook-Pro.local>
2018-03-02 23:41:27 +0000

    watch

--- database/dockerfiles/run.sh
+++ database/dockerfiles/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-postgraphile     -c postgres://origin_postgraphql:allegro1234@origingg.cjdraitfnk0j.us-east-1.rds.amazonaws.com:5432/originGG   --host 0.0.0.0   -s origin     --port 5000       --default-role origin_anonymous     --jwt-secret bbo9Q4jsIkfQ1gkpolQAKLXO4WZ-s3SEcvo3gHwfxCEM_IBSisDWzlwcmDKjVfH0     --token origin.jwt_token     -o
+postgraphile     -c postgres://origin_postgraphql:allegro1234@origingg.cjdraitfnk0j.us-east-1.rds.amazonaws.com:5432/originGG   --watch   --host 0.0.0.0   -s origin     --port 5000       --default-role origin_anonymous     --jwt-secret bbo9Q4jsIkfQ1gkpolQAKLXO4WZ-s3SEcvo3gHwfxCEM_IBSisDWzlwcmDKjVfH0     --token origin.jwt_token     -o
