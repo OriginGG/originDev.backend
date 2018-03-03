c374cb9db9c58738677ce298abbc803bedca1ba3
Stefan Walker <stefanwalker@Stefans-MacBook-Pro.local>
2018-03-03 10:14:58 +0000

    fixes

--- database/update_nginx.sh
+++ database/update_nginx.sh
@@ -7,5 +7,5 @@ docker stop nginx_proxy
 docker rm nginx_proxy
 docker rm $(docker ps -a -q)
 docker rmi $(docker images --quiet --filter "dangling=true")
-docker run --net=backend --name nginx_proxy --restart=always ${detached} -p 80:80 -p 443:443 -v ~/applications/hub/my_proxy.conf:/etc/nginx/conf.d/my_proxy.conf:ro -v /var/run/docker.sock:/tmp/docker.sock:ro -v ~/applications/hub/bootstrap/certs:/etc/nginx/certs jwilder/nginx-proxy
+docker run --net=backend --name nginx_proxy --restart=always ${detached} -p 80:80 -p 443:443 -v ~/applications/backend/database/my_proxy.conf:/etc/nginx/conf.d/my_proxy.conf:ro -v /var/run/docker.sock:/tmp/docker.sock:ro -v ~/applications/hub/bootstrap/certs:/etc/nginx/certs jwilder/nginx-proxy
 
