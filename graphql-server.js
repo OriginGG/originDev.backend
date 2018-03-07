eb4ca2b1b92837885c713622b1d535828c03dd5c
Stefan Walker <stefanwalker@Stefans-MacBook-Pro.local>
2018-03-07 12:32:14 +0000

    Fixes

---
+++ graphql-server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { postgraphql } = require('postgraphql');
+const config = require('./config/index');
+
+const app = express();
+
+
+app.use(postgraphql(config.postgraphile.host, config.postgraphile.schema_name, {
+    pgDefaultRole: config.postgraphile.default_role,
+    enableCors: true,
+    jwtSecret: config.postgraphile.jwt_secret,
+    jwtPgTypeIdentifier: config.postgraphile.jwt_token,
+    graphiql: config.postgraphile.graphiql
+}));
+
+app.listen(config.postgraphile.port || 5000);
--- server.js
+++ server.js
@@ -1,17 +1,50 @@
 const express = require('express');
-const { postgraphql } = require('postgraphql');
-
+const bodyParser = require('body-parser')
 const config = require('./config/index');
-
+const gql = require('graphql-tag');
+const axios = require('axios');
+const sa = require('superagent');
 
 const app = express();
 
-app.use(postgraphql(config.postgraphile.host, config.postgraphile.schema_name, {
-    pgDefaultRole: config.postgraphile.default_role,
-    enableCors: true,
-    jwtSecret: config.postgraphile.jwt_secret,
-    jwtPgTypeIdentifier: config.postgraphile.jwt_token,
-    graphiql: config.postgraphile.graphiql
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
 }));
 
+
+app.post('/update_theme', function (req, res) {
+
+    const q = `mutation updateTheme($themeName: String!, $themeData: String!) {
+        updateThemeByThemeName(input:{themeName:$themeName, themePatch: {
+            themeData: $themeData
+        }}) 
+        {
+            theme {
+            themeName
+            themeData
+            }
+        }
+    }`;
+
+    const { themeName } = req.body;
+    const { themeData } = req.body;
+    const x = JSON.parse(themeData);
+    const bd = {
+        "query": q,
+        "operationName": "updateTheme",
+        "variables": {
+            "themeName": themeName,
+            "themeData": JSON.stringify(x)
+        }
+    }
+
+    sa.post(config.graphql.graphql_url)
+        .send(bd)
+        .end((err, res) => {
+            res.send('hello world')
+            //TODO
+        });
+    // now we register the organisation
+})
 app.listen(config.server.port || 3001);
