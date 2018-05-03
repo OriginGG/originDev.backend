955bd8a9055c52fe086fe4d0a965a460bd97bfe3
Stefan Walker <stefan.walker@gmail.com>
2018-05-03 15:36:04 +0100

    Fix

--- graphql/database/migrations/20180502132540_origin.js
+++ graphql/database/migrations/20180502132540_origin.js
@@ -1,7 +1,7 @@
 exports.up = async function (knex) {
     return Promise.all([
         knex.schema.table('individual_users', function (table) {
-            table.text('bannerl_image_url');
+            table.text('banner_image_url');
             table.string('profile_image_url');
         })
     ])
