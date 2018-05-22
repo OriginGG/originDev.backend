fe8c32db61d159ce7d1573d947b2aee8f2020bec
Stefan Walker <stefan.walker@gmail.com>
2018-05-22 10:33:14 +0100

    Mail fixes

--- api/app/Controllers/Http/MailController.js
+++ api/app/Controllers/Http/MailController.js
@@ -6,11 +6,12 @@ const Env = use('Env')
 
 class MailController {
     async signup({ response, request }) {
-        const data = request.only(['email', 'name', 'password', 'admin_user', 'token', 'dev'])
+        const data = request.only(['id' ,'email', 'name', 'password', 'admin_user', 'token', 'dev'])
         if (data.admin_user === true || data.admin_user === 'true') {
+            const payload = Buffer.from(JSON.stringify(data), 'utf8').toString('hex');
             // let url = `http://localhost:3000/new_signup?token=${data.token}`;
             // if (!data.dev) {
-            const url = `http://origin.gg/new_signup?token=${data.token}&admin=true`;
+            const url = `http://origin.gg/new_signup?p=${payload}&admin=true`;
             // }
             await Mail.send('emails.signup', { organization_url: url, name: data.name }, (message) => {
                 message
@@ -19,7 +20,8 @@ class MailController {
                     .subject('Welcome to OriginGG')
             })
         } else {
-            const url = `http://origin.gg/new_signup_ind?&p=${v.password}&id=${data.id}&admin=false`;
+            const payload = Buffer.from(JSON.stringify(data), 'utf8').toString('hex');
+            const url = `http://origin.gg/new_signup_ind?&p=${payload}&admin=false`;
             // }
             await Mail.send('emails.signup_individual', { organization_url: url, name: data.name }, (message) => {
                 message
--- graphql/database/postgreSQL_schemas/4_origin_schema.sql
+++ graphql/database/postgreSQL_schemas/4_origin_schema.sql
@@ -1,9 +1,35 @@
 begin;
 
-drop function origin.authenticate_individual(
+drop function if exists origin.authenticate_individual(
   email text,
   password text
 );
+
+drop function if exists origin.register_user(
+  first_name text,
+  last_name text,
+  email text,
+  password text,
+  admin_user boolean
+);
+
+drop function if exists origin.individual_user_register(
+  first_name text,
+  last_name text,
+  email text,
+  password text
+);
+
+drop function if exists origin.individual_user_register(
+  first_name text,
+  last_name text,
+  email text,
+  password text,
+  authenticated boolean,
+  user_name text
+);
+
+
 DROP TYPE IF EXISTS origin.individual_auth_payload;
 DROP TYPE IF EXISTS individual_auth_account;
 
@@ -37,5 +63,49 @@ begin
   end if;
 end;
 $$ language plpgsql strict security definer;
+
+
+create function origin.register_user(
+  first_name text,
+  last_name text,
+  email text,
+  password text,
+  admin_user boolean
+) returns origin.users as $$
+declare
+  person origin.users;
+begin
+  insert into origin.users (first_name, last_name, organisation, email, password_hash, admin_user) values
+    (first_name, last_name, null, email, crypt(password, gen_salt('bf')), admin_user)
+    returning * into person;
+  return person;
+end;
+$$ language plpgsql strict security definer;
+
+
+create or replace function origin.individual_user_register(
+  first_name text,
+  last_name text,
+  email text,
+  password text,
+  authenticated boolean,
+  user_name text
+) returns origin.individual_users as $$
+declare
+  person origin.individual_users;
+begin
+  insert into origin.individual_users (first_name, last_name, email, password_hash, authenticated, username) values
+    (first_name, last_name, email, crypt(password, gen_salt('bf')), authenticated, user_name)
+    returning * into person;
+  return person;
+end;
+$$ language plpgsql strict security definer;
+
+
+grant execute on function origin.register_user(text, text, text, text, boolean) to origin_anonymous;
+
+grant execute on function origin.individual_user_register(text, text, text, text, boolean, text) to origin_anonymous;
+
+
 
 commit;
