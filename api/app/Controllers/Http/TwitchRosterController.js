18152a0193ec2cbefe6107e2957d12e943801635
Yukun <yukunpengba@gmail.com>
2018-06-28 15:49:52 -0500

    dependencies classes

--- api/app/Controllers/Http/TwitchRosterController.js
+++ api/app/Controllers/Http/TwitchRosterController.js
@@ -1,4 +1,14 @@
 // Load in Database and select roster individuals
+'use strict'
+
+const Env = use('Env');
+const Database = use('Database');
+const getTwitchuser = require('TwitchController.js')
+
+class TwitchController{
+
+
+}
 
 
 // use the gettiwchuser method to grab the user data in a for loop
