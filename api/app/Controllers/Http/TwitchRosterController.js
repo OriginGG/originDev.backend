cb9b6add6eeb716370ab69501fdfbf5dfed6bc2a
Yukun <yukunpengba@gmail.com>
2018-07-04 04:05:03 -0500

    TwitchGroupRoster

--- api/app/Controllers/Http/TwitchRosterController.js
+++ api/app/Controllers/Http/TwitchRosterController.js
@@ -3,58 +3,14 @@
 const Env = use('Env')
 const axios = require('axios');
 const Database = use('Database');
+const TwitchController = require('./TwitchController');
 
 class TwitchGroupController {
-    
-    
-    
-    readTwitchTokenFromServer() {
-        return new Promise(async (resolve, reject) => {
-            const url = `${Env.get('TWITCH_AUTH_API')}/oauth2/token?client_id=${Env.get('TWITCH_CLIENT_ID')}&client_secret=${Env.get('TWITCH_CLIENT_SECRET')}&grant_type=client_credentials`;
-            try {
-                const td = await axios.post(url);
-                resolve(td.data);
-            } catch (err) {
-                resolve(null);
-            }    
-        })
-    }
-    
-    
-    async getTwitchToken(session) {
-        const p = session.get('twitch_auth_object')
-        if (!p) {
-            const token = await this.readTwitchTokenFromServer();
-            session.put('twitch_auth_object', token);
-            const d = new Date();
-            const n = d.getTime();
-            const exp_time = n + token.expires_in;
-            session.put('twitch_auth_expiry', exp_time);
-            return token.access_token;
-        } else {
-            const tm = session.get('twitch_auth_expiry');
-            const d = new Date();
-            const n = d.getTime();
-            if (n > tm) {
-                const token = await this.readTwitchTokenFromServer();
-                session.put('twitch_auth_object', token);
-                const d = new Date();
-                const n = d.getTime();
-                const exp_time = n + token.expires_in;
-                session.put('twitch_auth_expiry', exp_time);
-                return token.access_token;
-            } else {
-                return p.access_token;
-            }
-        }
-        // first see if its in the session.
 
-    }
-    async getTwitchGroupInfo({ session, response, request }) {
-        const rosternames = await Database.select('*').from('domain_registration');
-        const data = request.only('name')
-        const url = `${Env.get('TWITCH_API')}/users?login=${data.name}`;
-        const token = await this.getTwitchToken(session);
+    async getTwitchGroupUserInfo({ session, response, input }) {
+      
+        const url = `${Env.get('TWITCH_API')}/users?login=${input}`;
+        const token = await TwitchController.getTwitchToken(session);
         try {
             const td = await axios.get(url, {
                 headers: {
@@ -62,7 +18,7 @@ class TwitchGroupController {
                 }
             });
             if (td.data.data.length > 0) {
-                response.json({ success: true, user: td.data.data[0] });
+                console.log())
             } else {
                 response.json({ success: false });
             }   
@@ -70,10 +26,46 @@ class TwitchGroupController {
             response.json({ success: false });
         }
     }
-    async getRosterIndividuals({}){
+    async DatabaseUpdates()
+    {
+    
+        var roster_id = await Database.select('id')
+        .from('rosters')
+        .where('sub_domain','enigma');
+        
+        
 
+      
+        var individual_ids = await Database.select('individual_id')
+        .from('roster_individuals')
+        .where('roster_id',`${roster_id[0].id}`);
+
+        for(var i = 0; i < individual_ids.length;i++)
+        {
+        
+        
+        var individualusers = await Database
+        .select('username','twitch_url').from('individual_users')
+        .where('id',`${individual_ids[i].individual_id}`);
+        
+        console.log(individualusers[i].twitch_url);
+
+        if(individualusers[i].twitch_url){
+            const tu = individualusers[i].twitch_url.substring(individualusers[i].twitch_url.lastIndexOf('/') + 1);
+            console.log(tu);
+            var views = TwitchController.getTwitchUserInfo(input=tu);
+        }
+       
+
+       
+        }
+        twitchJson.push(individualusers);
+
+
+        
+        console.log(twitchJson);
+       
     }
 
-
 }
-module.exports = TwitchController
+module.exports = TwitchGroupController
