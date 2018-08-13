f42a4e1df1e504f6781aae491d9aff4a56de831b
Yukun <yukunpengba@gmail.com>
2018-08-13 16:04:56 -0500

    Adding Exception Handling for TwitterController

--- api/app/Controllers/Http/TwitterController.js
+++ api/app/Controllers/Http/TwitterController.js
@@ -7,38 +7,44 @@ var Twitter = require('twitter');
 
 
 class TwitterController {
-  async getTwitterUserInfo({session,response,request}) {
+  async getTwitterUserInfo({session,response,request
+  }) {
     const data = request.only(['user']);
-   
+    try {
       const twitterData = await this.UserLookUp(data.user);
       response.json(twitterData);
-    
+    } catch (err) {
+      response.json({
+        success: false
+      });
     }
 
+  }
 
-    UserLookUp(user) {
-        var client = new Twitter({
-            consumer_key: `${Env.get('TWITTER_CONSUMER_KEY')}`,
-            consumer_secret: `${Env.get('TWITTER_CONSUMER_SECRET')}`,
-            access_token_key: '4310668813-IwYHz0BaZ2l9EvDrnsnJouH71ZaClhajKYkf9TY',
-            access_token_secret: 'XJsFyx24vStqsBfKOgrRMmVMeBVLu04urxhi5Ppz7qDts'
-          });
-          var params = {
-            screen_name: `${user}`
-          };
-        return new Promise(async(resolve, reject) => {
-            client.get('users/lookup', params,(error, data, response) => {
-                if (error) {
-                  reject(error);
-                } else {
-                  resolve(data);
-                }
-              });
-            });
-          }
+
+  UserLookUp(user) {
+    var client = new Twitter({
+      consumer_key: `${Env.get('TWITTER_CONSUMER_KEY')}`,
+      consumer_secret: `${Env.get('TWITTER_CONSUMER_SECRET')}`,
+      access_token_key: '4310668813-IwYHz0BaZ2l9EvDrnsnJouH71ZaClhajKYkf9TY',
+      access_token_secret: 'XJsFyx24vStqsBfKOgrRMmVMeBVLu04urxhi5Ppz7qDts'
+    });
+    var params = {
+      screen_name: `${user}`
+    };
+    return new Promise(async (resolve, reject) => {
+      client.get('users/lookup', params, (error, data, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(data);
         }
+      });
+    });
+  }
+}
 
 
 
 
-        module.exports = TwitterController
+module.exports = TwitterController
