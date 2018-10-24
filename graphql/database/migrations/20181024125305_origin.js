d0fd37a6cb1f47dfea5ec3d11a9102ea8f472438
Stefan Walker <stefan.walker@gmail.com>
2018-10-24 14:12:48 +0100

    Fixes 2

--- graphql/database/migrations/20181024125305_origin.js
+++ graphql/database/migrations/20181024125305_origin.js
@@ -2,17 +2,17 @@
 const fs = require('fs');
 
 exports.up = function (knex, Promise) {
-    console.log('new sql functions  19- begin');
+    console.log('new sql functions  20- begin');
     return new Promise((resolve) => {
         console.log(__dirname);
-        fs.readFile(`${__dirname}/../postgreSQL_schemas/19_origin_schema.sql`, 'utf8', function read(err, data) {
+        fs.readFile(`${__dirname}/../postgreSQL_schemas/20_origin_schema.sql`, 'utf8', function read(err, data) {
             if (err) {
                 throw err;
             }
             Promise.all([
                 knex.raw(data),
             ]).then(() => {
-                console.log('new sql functions 19');
+                console.log('new sql functions 20');
                 resolve(true);
             })
         });
---
+++ graphql/database/postgreSQL_schemas/20_origin_schema.sql
@@ -0,0 +1,9 @@
+begin;
+
+
+alter table origin.users enable row level security;
+
+create policy users_security on origin.users to origin_user,  origin_admin
+  using(organisation = current_setting('jwt.claims.organisation')::text)
+
+commit;
